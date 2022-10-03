6af1ac6ad618533652aecbc56b646c07ee161256
LaurentCNS <lo42fr@live.fr>
2022-10-03 14:39:18 +0200

    first commit

---
+++ python_intro.py
@@ -0,0 +1,15 @@
+def hi(name):
+    print('Salut ' + name + '!')
+
+girls = ['Vanessa','Pauline','Anne', 'Julie', 'Emma', 'Dorothee']
+
+# for item in girls:
+#     hi(item)
+#     print('next')
+
+for i in range(1,6):
+    print(i)
+
+
+
+
